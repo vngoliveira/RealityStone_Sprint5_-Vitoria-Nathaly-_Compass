4f38331b20bf4f9c0712ffe5c157cf1659b78c98
vngoliveira <vivi40195@gmail.com>
2022-05-27 01:51:40 -0300

    Capabilities

--- SitePrism&Cucumber/features/support/env.rb
+++ SitePrism&Cucumber/features/support/env.rb
@@ -10,5 +10,16 @@ ENVIRONMENT_CONFIG = YAML.load_file(File.dirname(__FILE__) + "/environment/#{ENV
 puts ENVIRONMENT_CONFIG
 URL = ENVIRONMENT_CONFIG['url']
 
-Capybara.default_driver = :selenium_chrome
+Capybara.register_driver :my_chrome do |app|
+    caps = Selenium::WebDriver::Remote::Capabilities.chrome("goog:chromeOptions" => 
+    {"args" => ["--incognito", "--start-maximized", "--window-size=1420,835"]})
+
+    if ENV['HEADLESS']
+        caps['goog:chromeOptions']['args'] << '--headless'
+    end
+
+    Capybara::Selenium::Driver.new(app, :browser => :chrome, capabilities: caps)
+end
+
+Capybara.default_driver = :my_chrome
 Capybara.app_host = URL
