876dee40ab1bafaf6270b967f92ccd5ac6af4c58
vngoliveira <vivi40195@gmail.com>
2022-05-27 01:16:13 -0300

    Execução multi-ambientes

--- SitePrism&Cucumber/features/support/env.rb
+++ SitePrism&Cucumber/features/support/env.rb
@@ -5,5 +5,10 @@ require 'site_prism'
 require 'selenium-webdriver'
 require 'rspec'
 
+ENVIRONMENT = ENV['ENVIRONMENT']
+ENVIRONMENT_CONFIG = YAML.load_file(File.dirname(__FILE__) + "/environment/#{ENVIRONMENT}.yml")
+puts ENVIRONMENT_CONFIG
+URL = ENVIRONMENT_CONFIG['url']
+
 Capybara.default_driver = :selenium_chrome
-Capybara.app_host = 'https://www.google.com.br'
+Capybara.app_host = URL
